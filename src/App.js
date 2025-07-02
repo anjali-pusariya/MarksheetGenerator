6012d195dc8bb9b7ddbb17930e1d58be011bb578
anjali-pusariya <anjalipusariya25@gmail.com>
2025-07-02 15:44:55 +0530

    Updated project with latest changes

--- src/App.js
+++ src/App.js
@@ -24,38 +24,60 @@
 
 
 
+// import React from 'react'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import EnterDetail from './Components/Marksheet/EnterDetail'
+// import Marksheet from './Components/Marksheet/Marksheet'
+// import { Table } from 'antd'
+
+// function App() {
+//   const datasource=[
+//     {key:1,"name":"anjali","age":20}
+// ]
+// const columns=[
+//   {
+// title:"Name",
+// dataIndex:"name",
+// key:'name'
+//   },
+//   {
+// title:"Age",
+// dataIndex:"age",
+// key:'age'
+//   },
+
+// ]
+//   return (
+//     <div>
+//       <BrowserRouter >
+//       <Routes>
+//         <Route path='/' element={<EnterDetail/>}/>
+//         <Route path='/Marksheet' element={<Marksheet/>}/>
+//       </Routes>
+//       </BrowserRouter>
+//       {/* <Table dataSource={datasource} columns={columns}/> */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import EnterDetail from './Components/Marksheet/EnterDetail'
 import Marksheet from './Components/Marksheet/Marksheet'
-import { Table } from 'antd'
 
 function App() {
-  const datasource=[
-    {key:1,"name":"anjali","age":20}
-]
-const columns=[
-  {
-title:"Name",
-dataIndex:"name",
-key:'name'
-  },
-  {
-title:"Age",
-dataIndex:"age",
-key:'age'
-  },
-
-]
   return (
     <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<EnterDetail/>}/>
-        <Route path='/Marksheet' element={<Marksheet/>}/>
-      </Routes>
+      <BrowserRouter basename="/MarksheetGenerator">
+        <Routes>
+          <Route path="/" element={<EnterDetail />} />
+          <Route path="/Marksheet" element={<Marksheet />} />
+        </Routes>
       </BrowserRouter>
-      {/* <Table dataSource={datasource} columns={columns}/> */}
     </div>
   )
 }
